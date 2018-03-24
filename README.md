e25289cee2b21583c780f1bc0e00c7e96684f564
Florian Ludwig <f.ludwig@greyrook.com>
2018-03-24 22:01:07 +0100

    remove ticket_id usage to download source
    The source is open source and public domain.
    
    https://github.com/jasonrohrer/OneLife/blob/master/build/source/OneLife_Live_UnixSource/pullAndBuildLatest

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 This will let you easily bring up a container running a One Hour One Life game server.
 
-## Setup for build
-
-You will need to add your ticket ID you receieved when you purchased the game (it's a long string comprised of 4 groups of 5 alpha-numeric characters) to the container configurations.  Save a file at the repo root called `ticketID.cfg` and place just your ticket ID in that file.
-
 ## Build the image
 
 You will need to have Docker installed to build and run the image.  From the repo root in your terminal run:
--- docker/fetch-and-compile.sh
+++ docker/fetch-and-compile.sh
@@ -1,39 +1,115 @@
-echo 'TICKET_ID in env var?' ${TICKET_ID-no}
+#!/bin/sh
 
-if [ ! "$TICKET_ID" ]; then
-	if [ ! -e /opt/src/ticketID.cfg ]; then
-		echo "Place your ticket ID in a ticketID.cfg at the repo root"
-		exit 1
-	fi
-
-	TICKET_ID=$(cat /opt/src/ticketID.cfg)
-
-	if [ ! "$TICKET_ID" ]; then
-		echo "No TICKET_ID defined in ticketID.cfg"
-		exit 2
-	fi
+if [ ! -e minorGems ]
+then
+	git clone https://github.com/jasonrohrer/minorGems.git
 fi
 
-echo TICKET_ID: $TICKET_ID
-
-SOURCE_URL=$(curl "http://onehouronelife.com/ticketServer/server.php?action=show_downloads&ticket_id=$TICKET_ID" | egrep -io 'http:[^"]+?source[^"]+?\.tar\.gz')
-
-if [ ! "$SOURCE_URL" ]; then
-	echo "No SOURCE_URL found.  Check that TICKET_ID is valid"
-	exit 3
+if [ ! -e OneLife ]
+then
+	git clone https://github.com/jasonrohrer/OneLife.git
 fi
 
-echo Source: $SOURCE_URL
+if [ ! -e OneLifeData7 ]
+then
+	git clone https://github.com/jasonrohrer/OneLifeData7.git
+fi
 
-mkdir -p /opt/game
-#mkdir -p /opt/data
-cd /opt/game
 
-curl -o source.tar.gz $SOURCE_URL
+cd minorGems
+git fetch
+latestTaggedVersion=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags | sed -e 's/OneLife_v//'`
+git checkout -q OneLife_v$latestTaggedVersion
 
-tar xvzf source.tar.gz
 
-./OneLife_Live4_UnixSource/pullAndBuildLatest
+cd ../OneLife
+git fetch
+latestTaggedVersionA=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags | sed -e 's/OneLife_v//'`
+git checkout -q OneLife_v$latestTaggedVersionA
+
+
+cd ../OneLifeData7
+git fetch
+latestTaggedVersionB=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags | sed -e 's/OneLife_v//'`
+git checkout -q OneLife_v$latestTaggedVersionB
+
+rm */cache.fcz
+
+
+latestVersion=$latestTaggedVersionB
+
+
+if [ $latestTaggedVersionA -gt $latestTaggedVersionB ]
+then
+	latestVersion=$latestTaggedVersionA
+fi
+
+
+
+cd ..
+
+
+if [ ! -h animations ]
+then
+	ln -s OneLifeData7/animations .
+fi
+
+
+if [ ! -h categories ]
+then
+	ln -s OneLifeData7/categories .
+fi
+
+
+if [ ! -h ground ]
+then
+	ln -s OneLifeData7/ground .
+fi
+
+
+if [ ! -h music ]
+then
+	ln -s OneLifeData7/music .
+fi
+
+
+if [ ! -h objects ]
+then
+	ln -s OneLifeData7/objects .
+fi
+
+
+if [ ! -h sounds ]
+then
+	ln -s OneLifeData7/sounds .
+fi
+
+
+if [ ! -h sprites ]
+then
+	ln -s OneLifeData7/sprites .
+fi
+
+
+if [ ! -h transitions ]
+then
+	ln -s OneLifeData7/transitions .
+fi
+
+
+if [ ! -h dataVersionNumber.txt ]
+then
+	ln -s OneLifeData7/dataVersionNumber.txt .
+fi
+
+
+
+
+cp OneLife/build/source/runToBuild .
+
+
+./runToBuild 1
+
 
 cd OneLife/server
 
