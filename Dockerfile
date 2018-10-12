308cd7c01f95a6d0453e69a1d86af1ee551b1c33
Andrew Stoneman <andrew@pumpkinpirate.info>
2018-10-12 15:04:45 -0700

    Back to a small final image!  This time using busybox:glibc.

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,19 @@
 FROM buildpack-deps:latest AS build
 
-ADD ./docker/fetch-and-compile.sh /opt/
-ADD ./docker/patches /opt/patches
+ADD ./docker/fetch-and-compile.sh /build/
+ADD ./docker/patches /build/patches
 
-RUN cd opt && ./fetch-and-compile.sh
+RUN /build/fetch-and-compile.sh
 
+ADD ./docker/run-server.sh /dist/
 
+FROM busybox:glibc
 
-FROM ubuntu:latest
-
-COPY --from=build /opt/OneLife/server /opt/OneLife/server
-COPY --from=build /opt/OneLifeData7/categories /opt/OneLifeData7/categories
-COPY --from=build /opt/OneLifeData7/objects /opt/OneLifeData7/objects
-COPY --from=build /opt/OneLifeData7/transitions /opt/OneLifeData7/transitions
-COPY --from=build /opt/OneLifeData7/dataVersionNumber.txt /opt/OneLifeData7/dataVersionNumber.txt
-COPY --from=build /opt/versionNumber.txt /opt/versionNumber.txt
-
-ADD ./docker/run-server.sh /opt/
+COPY --from=build /dist /usr
 
 EXPOSE 8005
-VOLUME /opt/OneLife/server/data
+VOLUME /usr/OneLife/server/data
 
-WORKDIR /opt/OneLife/server
-CMD /opt/run-server.sh
+WORKDIR /usr/OneLife/server
+CMD /usr/run-server.sh
 STOPSIGNAL SIGTSTP
--- docker/fetch-and-compile.sh
+++ docker/fetch-and-compile.sh
@@ -1,28 +1,17 @@
 #!/bin/sh
 set -e
 
-if [ ! -e minorGems ]
-then
-	git clone https://github.com/jasonrohrer/minorGems.git
-fi
+cd /build
 
-if [ ! -e OneLife ]
-then
-	git clone https://github.com/jasonrohrer/OneLife.git
-fi
-
-if [ ! -e OneLifeData7 ]
-then
-	git clone https://github.com/jasonrohrer/OneLifeData7.git
-fi
-
-
+### Checkout and patch minorGems ###
+cd /build
+git clone https://github.com/jasonrohrer/minorGems.git
 cd minorGems
 git fetch --tags
 minorGemsVersion=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//'`
 git checkout -q OneLife_v$minorGemsVersion
 
-for patch in /opt/patches/minorGems/*.patch
+for patch in /build/patches/minorGems/*.patch
 do
 	if [ -e $patch ]
 	then
@@ -31,12 +20,15 @@ do
 	fi
 done
 
-cd ../OneLife
+### Checkout and patch OneLife ###
+cd /build
+git clone https://github.com/jasonrohrer/OneLife.git
+cd OneLife
 git fetch --tags
 oneLifeVersion=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//'`
 git checkout -q OneLife_v$oneLifeVersion
 
-for patch in /opt/patches/OneLife/*.patch
+for patch in /build/patches/OneLife/*.patch
 do
 	if [ -e $patch ]
 	then
@@ -45,12 +37,15 @@ do
 	fi
 done
 
-cd ../OneLifeData7
+### Checkout and patch OneLifeData7 ###
+cd /build
+git clone https://github.com/jasonrohrer/OneLifeData7.git
+cd OneLifeData7
 git fetch --tags
 oneLifeDataVersion=`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//'`
 git checkout -q OneLife_v$oneLifeDataVersion
 
-for patch in /opt/patches/OneLifeData/*.patch
+for patch in /build/patches/OneLifeData/*.patch
 do
 	if [ -e $patch ]
 	then
@@ -59,23 +54,34 @@ do
 	fi
 done
 
-latestVersion=$oneLifeVersion
-
-
-if [ $oneLifeDataVersion -gt $oneLifeVersion ]
-then
-	latestVersion=$oneLifeDataVersion
-fi
-
-
-cd ../OneLife/server
+### Compile the server ###
+cd /build/OneLife/server
 
 ./configure 1
 make
 ./makeDBConvert
 
 echo $oneLifeVersion > serverCodeVersionNumber.txt
-echo $latestVersion > /opt/versionNumber.txt
-
 mkdir data
 mkdir data/settings
+
+### Copy the files the server will actually need from /build to /dist ###
+mkdir /dist
+mkdir /dist/OneLife
+cp -R /build/OneLife/server /dist/OneLife/server
+
+mkdir /dist/OneLifeData7
+cp -R /build/OneLifeData7/categories /dist/OneLifeData7/categories
+cp -R /build/OneLifeData7/objects /dist/OneLifeData7/objects
+cp -R /build/OneLifeData7/transitions /dist/OneLifeData7/transitions
+cp -R /build/OneLifeData7/dataVersionNumber.txt /dist/OneLifeData7/dataVersionNumber.txt
+
+latestVersion=$oneLifeVersion
+if [ $oneLifeDataVersion -gt $oneLifeVersion ]
+then
+	latestVersion=$oneLifeDataVersion
+fi
+echo $latestVersion > /dist/versionNumber.txt
+
+mkdir /dist/lib
+ldd /dist/OneLife/server/OneLifeServer | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' /dist/lib
